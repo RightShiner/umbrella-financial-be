23bbe20a262f75d7cb1523d514a38c4382115076
RightShiner <brilight.shiner@gmail.com>
2023-02-10 18:28:10 -0500

    base setup

---
+++ src/Context.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+export type Context = {
+    response: Response;
+    request: Request;
+    prismaClient: PrismaClient
+}
