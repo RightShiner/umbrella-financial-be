9a1fe39e7f4e1ab8c55884b5be0ba265ddf01614
danielfournier-tech <81045479+danielfournier-tech@users.noreply.github.com>
2023-02-16 22:01:13 -0500

    Update route.ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -6,9 +6,25 @@ import { Commission } from '../Models/commission';
 export const router = express.Router();
 
 
-router.post("/user", (req, res) => UserClient.handlePostRequest({ request: req, response: res }));
-router.post("/user/login", (req, res) => UserClient.login({ request: req, response: res }));
-router.get("/user/:id", (req, res) => UserClient.handleGetRequest({ request: req, response: res }));
+router.post("/users", (req, res) => UserClient.handlePostRequest({ request: req, response: res }));
+router.post("/users/login", (req, res) => UserClient.login({ request: req, response: res }));
+router.get("/users/:id", (req, res, next) => handleApiError({ request: req, response: res, next: next }, UserClient.handleGetUserByIdRequest));
+router.get("/users/:id/sales", (req, res, next) => handleApiError({ request: req, response: res, next: next }, UserClient.getAllSalesForUser));
+router.get("/users", (req, res, next) => handleApiError({ request: req, response: res, next: next }, UserClient.handleGetRequest));
+
+router.post("/sales", (req, res, next) => handleApiError({ request: req, response: res, next: next }, SaleClient.handlePostRequest));
+router.get("/sales/:id", (req, res, next) => handleApiError({ request: req, response: res, next: next }, SaleClient.handleGetSaleByIdRequest));
+router.get("/sales", (req, res, next) => handleApiError({ request: req, response: res, next: next }, SaleClient.handleGetRequest));
+router.delete("/sales", (req, res, next) => handleApiError({ request: req, response: res, next: next }, SaleClient.deleteAllSales));
+
+router.post("/products", (req, res, next) => handleApiError({ request: req, response: res, next: next }, ProductClient.handlePostRequest));
+router.get("/products/:id", (req, res, next) => handleApiError({ request: req, response: res, next: next }, ProductClient.handleGetProductByIdRequest));
+router.get("/products", (req, res, next) => handleApiError({ request: req, response: res, next: next }, ProductClient.handleGetRequest));
+
+router.post("/commission-plans", (req, res, next) => handleApiError({ request: req, response: res, next: next }, CommissionPlanClient.handlePostRequest));
+router.get("/commission-plans/:id", (req, res, next) => handleApiError({ request: req, response: res, next: next }, CommissionPlanClient.handleGetCommissionPlanByIdRequest));
+router.get("/commission-plans", (req, res, next) => handleApiError({ request: req, response: res, next: next }, CommissionPlanClient.handleGetRequest));
+
 router.post("/totalcom", (req, res) => Commission.totalcom({request:req , response: res}));
 router.post("/tranInit", (req, res) => Commission.tranInit({request:req , response: res}));
 router.post("/customerInit", (req, res) => Commission.custInit({request:req , response: res}));
